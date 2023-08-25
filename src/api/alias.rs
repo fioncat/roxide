7a53e5426318aa340d714a709e72a7084f89dd6e
fioncat <lazycat7706@gmail.com>
2023-08-26 04:15:01 +0800

    feat(alias): api list show alias name

--- src/api/alias.rs
+++ src/api/alias.rs
@@ -9,18 +9,25 @@ pub struct Alias {
 
     owner_map: HashMap<String, String>,
     repo_map: HashMap<String, HashMap<String, String>>,
+
+    repo_map_revert: HashMap<String, HashMap<String, String>>,
 }
 
 impl Provider for Alias {
-    fn get_repo(&self, owner: &str, name: &str) -> Result<ApiRepo> {
-        let owner = self.alias_owner(owner);
-        let name = self.alias_repo(owner, name);
+    fn get_repo(&self, raw_owner: &str, raw_name: &str) -> Result<ApiRepo> {
+        let owner = self.alias_owner(raw_owner);
+        let name = self.alias_repo(owner, raw_name);
         self.upstream.get_repo(owner, name)
     }
 
     fn list_repos(&self, owner: &str) -> Result<Vec<String>> {
         let owner = self.alias_owner(owner);
-        self.upstream.list_repos(owner)
+        let names = self.upstream.list_repos(owner)?;
+        let names = names
+            .into_iter()
+            .map(|name| self.raw_repo(owner, name))
+            .collect();
+        Ok(names)
     }
 
     fn get_merge(&self, mut merge: MergeOptions) -> Result<Option<String>> {
@@ -50,10 +57,22 @@ impl Alias {
         repo_map: HashMap<String, HashMap<String, String>>,
         upstream: Box<dyn Provider>,
     ) -> Box<dyn Provider> {
+        let repo_map_revert: HashMap<_, _> = repo_map
+            .iter()
+            .map(|(owner, map)| {
+                (
+                    owner.clone(),
+                    map.iter()
+                        .map(|(key, val)| (val.clone(), key.clone()))
+                        .collect(),
+                )
+            })
+            .collect();
         Box::new(Alias {
             owner_map,
             repo_map,
             upstream,
+            repo_map_revert,
         })
     }
 
@@ -68,6 +87,15 @@ impl Alias {
         if let Some(map) = self.repo_map.get(owner) {
             if let Some(name) = map.get(name) {
                 return name.as_str();
+            }
+        }
+        name
+    }
+
+    fn raw_repo(&self, owner: &str, name: String) -> String {
+        if let Some(map) = self.repo_map_revert.get(owner) {
+            if let Some(raw) = map.get(&name) {
+                return raw.clone();
             }
         }
         name
