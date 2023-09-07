96212aa1d9d6431ab96d81b71c25587b75833545
wenqian <lazycat7706@gmail.com>
2023-09-07 16:33:41 +0800

    feat(home): add url option to locate repo by url (#57)

--- src/cmd/run/home.rs
+++ src/cmd/run/home.rs
@@ -3,16 +3,17 @@ use std::io::ErrorKind;
 use std::path::PathBuf;
 use std::rc::Rc;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use clap::Args;
+use reqwest::Url;
 
 use crate::cmd::Run;
 use crate::config::types::Remote;
-use crate::confirm;
 use crate::repo::database::Database;
-use crate::repo::query::{Query, SelectOptions};
+use crate::repo::query::{parse_owner, Query, SelectOptions};
 use crate::repo::types::Repo;
 use crate::term::{Cmd, Workflow};
+use crate::{config, confirm};
 
 /// Print the home path of a repo, recommand to use `zz` command instead.
 #[derive(Args)]
@@ -27,17 +28,28 @@ pub struct HomeArgs {
     /// If true, the cache will not be used when calling the API search.
     #[clap(long, short)]
     pub force: bool,
+
+    /// Use url to locate repo.
+    #[clap(long, short)]
+    pub url: Option<String>,
 }
 
 impl Run for HomeArgs {
     fn run(&self) -> Result<()> {
         let mut db = Database::read()?;
-        let query = Query::new(&db, self.query.clone());
-        let (remote, repo, exists) = query.select(
-            SelectOptions::new()
-                .with_force(self.force)
-                .with_search(self.search),
-        )?;
+
+        let (remote, repo, exists) = match self.url.as_ref() {
+            Some(url) => self.select_from_url(&db, url)?,
+            None => {
+                let query = Query::new(&db, self.query.clone());
+                query.select(
+                    SelectOptions::new()
+                        .with_force(self.force)
+                        .with_search(self.search),
+                )?
+            }
+        };
+
         if !exists {
             confirm!(
                 "Do you want to create {}/{}",
@@ -65,6 +77,8 @@ impl Run for HomeArgs {
 }
 
 impl HomeArgs {
+    const GITHUB_DOMAIN: &str = "github.com";
+
     fn create_dir(&self, remote: &Remote, repo: &Rc<Repo>, dir: &PathBuf) -> Result<()> {
         if let Some(_) = remote.clone {
             return self.clone(remote, repo, dir);
@@ -109,5 +123,75 @@ impl HomeArgs {
                 .check()?;
         }
         Ok(())
+    }
+
+    fn select_from_url(&self, db: &Database, url: &String) -> Result<(Remote, Rc<Repo>, bool)> {
+        let url = Url::parse(url).with_context(|| format!("Invalid url {url}"))?;
+        let domain = match url.domain() {
+            Some(domain) => domain,
+            None => bail!("Missing domain in url"),
+        };
+        let remote_names = config::list_remotes();
+
+        let mut target_remote: Option<Remote> = None;
+        let mut is_gitlab = false;
+        for name in remote_names {
+            let remote = config::must_get_remote(name)?;
+            let remote_domain = match remote.clone.as_ref() {
+                Some(domain) => domain,
+                None => continue,
+            };
+
+            if remote_domain != domain {
+                continue;
+            }
+
+            if remote_domain != Self::GITHUB_DOMAIN {
+                is_gitlab = true;
+            }
+            target_remote = Some(remote);
+            break;
+        }
+
+        let remote = match target_remote {
+            Some(remote) => remote,
+            None => bail!("Could not find remote match domain {domain}"),
+        };
+
+        let path_iter = match url.path_segments() {
+            Some(iter) => iter,
+            None => bail!("Invalid url, path could not be empty"),
+        };
+
+        let mut parts = Vec::new();
+        for part in path_iter {
+            if is_gitlab {
+                if part == "-" {
+                    break;
+                }
+                parts.push(part);
+                continue;
+            }
+
+            if parts.len() == 2 {
+                break;
+            }
+            parts.push(part);
+        }
+
+        if parts.len() < 2 {
+            bail!("Invalid url, should be in a repo");
+        }
+
+        let query = parts.join("/");
+        let (owner, name) = parse_owner(&query);
+
+        match db.get(&remote.name, &owner, &name) {
+            Some(repo) => Ok((remote, repo, true)),
+            None => {
+                let repo = Repo::new(&remote.name, &owner, &name, None);
+                Ok((remote, repo, false))
+            }
+        }
     }
 }
