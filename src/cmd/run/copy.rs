079087b68e92331fda67f96ce64a5f677d637a94
fioncat <lazycat7706@gmail.com>
2023-11-21 16:03:01 +0800

    feat(copy): Use default name and owner when no provided

--- src/cmd/run/copy.rs
+++ src/cmd/run/copy.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::io::ErrorKind;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use clap::Args;
 
 use crate::cmd::Run;
@@ -19,7 +19,7 @@ pub struct CopyArgs {
     pub remote: String,
 
     /// The target repo name.
-    pub name: String,
+    pub name: Option<String>,
 
     /// Mark repo as tmp.
     #[clap(long, short)]
@@ -30,18 +30,50 @@ impl Run for CopyArgs {
     fn run(&self) -> Result<()> {
         let mut db = Database::read()?;
 
-        let (owner, name) = query::parse_owner(&self.name);
+        let (mut owner, mut name) = match self.name.as_ref() {
+            Some(name) => query::parse_owner(name),
+            None => (String::new(), String::new()),
+        };
+        if owner.is_empty() {
+            owner = String::from("copy");
+        }
+        if name.is_empty() {
+            let dir = config::current_dir();
+            let dir_name = match dir.file_name() {
+                Some(dir_name) => match dir_name.to_str() {
+                    Some(dir_name) => dir_name,
+                    None => bail!("Invalid current directory name"),
+                },
+                None => bail!("You are not in a valid directory"),
+            };
+            name = format!("{dir_name}-copy");
+        }
+        let repo = Repo::new(&self.remote, &owner, &name, None);
 
         if let Some(_) = db.get(&self.remote, &owner, &name) {
-            confirm!("Do you want to overwrite the repo");
+            confirm!("Do you want to overwrite the repo {}", repo.full_name());
+        } else {
+            confirm!("Do you want to create {}", repo.full_name());
         }
-
-        let remote = config::must_get_remote(&self.remote)?;
 
         let git_remote = GitRemote::new();
         let clone_url = git_remote.get_url()?;
-
-        let repo = Repo::new(&self.remote, &owner, &name, None);
+        let user = match Cmd::git(&["config", "user.name"])
+            .with_desc("Get current user")
+            .execute()?
+            .checked_read()
+        {
+            Ok(user) => Some(user),
+            Err(_) => None,
+        };
+        let email = match Cmd::git(&["config", "user.email"])
+            .with_desc("Get current email")
+            .execute()?
+            .checked_read()
+        {
+            Ok(email) => Some(email),
+            Err(_) => None,
+        };
 
         if self.tmp {
             let mut tmp_mark = TmpMark::read()?;
@@ -66,13 +98,13 @@ impl Run for CopyArgs {
             .with_desc(format!("Copy to {}", repo.full_name()))
             .execute()?
             .check()?;
-        if let Some(user) = &remote.user {
+        if let Some(user) = user {
             Cmd::git(&["-C", path.as_str(), "config", "user.name", user.as_str()])
                 .with_desc(format!("Set user to {}", user))
                 .execute()?
                 .check()?;
         }
-        if let Some(email) = &remote.email {
+        if let Some(email) = email {
             Cmd::git(&["-C", path.as_str(), "config", "user.email", email.as_str()])
                 .with_desc(format!("Set email to {}", email))
                 .execute()?
