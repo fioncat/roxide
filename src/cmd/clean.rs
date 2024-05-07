5f163fa6eecd4c7e3c7668f016a4f1584f025651
fioncat <lazycat7706@gmail.com>
2024-05-07 15:23:44 +0800

    feat(check): add remote api check, remove check in clean

--- src/cmd/clean.rs
+++ src/cmd/clean.rs
@@ -8,76 +8,19 @@ use clap::Args;
 use crate::cmd::Run;
 use crate::config::Config;
 use crate::repo::database::Database;
-use crate::{api, info, term, utils};
+use crate::{info, term, utils};
 
 /// Collect and remove unused garbage.
 #[derive(Args)]
-pub struct CleanArgs {
-    /// Skip cleaning remote api.
-    #[clap(long)]
-    pub no_remote: bool,
-
-    /// When calling the remote API, ignore caches that are not expired.
-    #[clap(long, short)]
-    pub force: bool,
-}
+pub struct CleanArgs {}
 
 impl Run for CleanArgs {
     fn run(&self, cfg: &Config) -> Result<()> {
-        if !self.no_remote {
-            self.clean_remote(cfg)?;
-        }
-
         self.clean_orphan(cfg)
     }
 }
 
 impl CleanArgs {
-    fn clean_remote(&self, cfg: &Config) -> Result<()> {
-        let mut db = Database::load(cfg)?;
-
-        let remotes = cfg.list_remotes();
-        let mut to_remove = Vec::new();
-        for remote_name in remotes {
-            let remote = match cfg.get_remote(&remote_name) {
-                Some(remote) => remote,
-                None => continue,
-            };
-
-            if remote.provider.is_none() {
-                continue;
-            }
-
-            let provider = api::build_provider(cfg, &remote, self.force)
-                .with_context(|| format!("build provider for remote '{remote_name}'"))?;
-
-            let repos = db.list_by_remote(&remote_name, &None);
-
-            for repo in repos {
-                info!("Check remote api for {}", repo.name_with_remote());
-                if let Err(err) = provider.get_repo(&repo.owner, &repo.name) {
-                    eprintln!("Repo {} has remote error: {err:#}", repo.name_with_remote());
-                    let ok = term::confirm("Do you want to remove it")?;
-                    if ok {
-                        to_remove.push(repo.update());
-                    }
-                }
-            }
-        }
-        if to_remove.is_empty() {
-            eprintln!("No repo to remove");
-            return Ok(());
-        }
-
-        for repo in to_remove {
-            let path = repo.get_path(cfg);
-            utils::remove_dir_recursively(path)?;
-            db.remove(repo);
-        }
-
-        db.save()
-    }
-
     fn clean_orphan(&self, cfg: &Config) -> Result<()> {
         let db = Database::load(cfg)?;
         let root = cfg.get_workspace_dir().clone();
