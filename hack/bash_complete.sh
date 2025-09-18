a8306cc430333a9482ceb58ef605078a1bdd19d5
fioncat <lazycat7706@gmail.com>
2025-09-18 18:46:43 +0800

    feat: add base complete support

--- hack/bash_complete.sh
+++ hack/bash_complete.sh
@@ -1,15 +1,35 @@
 _rox() {
-	local cmd=${COMP_WORDS[0]}
-	local cmp_args=("${COMP_WORDS[@]:1}")
-	local items=($($cmd complete "${cmp_args[@]}" 2>/dev/null))
+	local cmd="${COMP_WORDS[0]}"
+	local complete_index=$COMP_CWORD
+	local items=($(ROXIDE_INIT="bash" ROXIDE_COMPLETE_INDEX="$complete_index" $cmd "${COMP_WORDS[@]:1}" 2>/dev/null))
 
-	local flags=${items[0]}
-	if [[ $flags -eq "1" ]]; then
-		# No space
-		compopt -o nospace
+	if [ ${#items[@]} -eq 0 ]; then
+		return
 	fi
-	local items=(${items[@]:1})
-	COMPREPLY=($(compgen -W "${items[*]}" -- "${COMP_WORDS[COMP_CWORD]}"))
+
+	local flags="${items[0]}"
+	local items=("${items[@]:1}")
+
+	case "${flags}" in
+	"0")
+		# Files
+		COMPREPLY=($(compgen -f -- "${COMP_WORDS[COMP_CWORD]}"))
+		;;
+	"1")
+		# Directories
+		COMPREPLY=($(compgen -d -- "${COMP_WORDS[COMP_CWORD]}"))
+		;;
+	"2")
+		# With space
+		COMPREPLY=($(compgen -W "${items[*]}" -- "${COMP_WORDS[COMP_CWORD]}"))
+		;;
+	"3")
+		# No space
+		COMPREPLY=($(compgen -W "${items[*]}" -- "${COMP_WORDS[COMP_CWORD]}"))
+		# Remove trailing space by setting compopt
+		compopt -o nospace
+		;;
+	esac
 }
 
-complete -o default -F _rox rox
+complete -F _rox rox
